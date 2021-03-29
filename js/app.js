e00266e57bd714b4180420a8b303cfaf6650af70
PhephisileN <40567433+PhephisileN@users.noreply.github.com>
2021-03-29 12:06:31 -1100

    first Changes

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const prevBtn = document.queryCommandEnabled('.prev');
+const nextBtn = document.queryCommandEnabled('.next');
+const galleryImgs = document.querySelectorAll('.gallery-img');
+let currentlySelected = 0;
+
+//events 
+
+prevBtn.addEventListener('click', function(){
+
+});
+
+netxBtn.addEventListener('click', function(){
+
+    galleryImgs[currentlySelected].classList.remove('active');
+    currentlySelected++;
+    
+    galleryImgs[currentlySelected].classList.add('active');
+    prevBtn.disabled = false;
+
+    if(galleryImgs.length ===currentlySelected + 1){
+        nextBtn.disabled = true;
+    }
+
+    
+});
